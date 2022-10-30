cc9d3fbf7555b57c3896919890ba6684f84c70b0
Tamerlan <dzhangtim@list.ru>
2022-10-30 19:12:43 +0300

    new programm3

---
+++ problem3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+int n = int.Parse(Console.ReadLine());
+
+if (n > 0){
+    for (int i = 1; i <= n; i++){
+        if (i % 2 == 0){
+             Console.WriteLine(i);
+             }
+   }
+
+} else (n < 0){
+for (int i = 1; i >= n; i--){
+    if (i % 2 == 0){
+        Console.WriteLine(i);
+    }
+}
+
+}
