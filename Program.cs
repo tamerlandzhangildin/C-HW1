c95660aea0e5251e0654a3d8419108f060b5fdb8
Tamerlan <dzhangtim@list.ru>
2022-10-30 18:10:50 +0300

    new programm

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+ // Console.WriteLine("Hello, World!");
+
+ int i = int.Parse(Console.ReadLine());
+ int j = int.Parse(Console.ReadLine());
+ int max = 0;
+ int min = 0;
+
+ if (i > j)
+ {
+      max = i;
+      min = j;
+ } else
+ {
+    max = j;
+    min = i;
+ }
+
+ Console.WriteLine("min: " + min + " max: " + max);
+
+
+
