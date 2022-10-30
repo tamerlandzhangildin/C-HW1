588892d0e8c168828227ee4ef8e70cd2f7e3a8ab
Tamerlan <dzhangtim@list.ru>
2022-10-30 18:30:54 +0300

    new programm 2

---
+++ problem2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int i = int.Parse(Console.ReadLine());
+ int j = int.Parse(Console.ReadLine());
+ int k = int.Parse(Console.ReadLine());
+ 
+ int max = i;
+
+ if (j > max){
+      max = j;
+    } 
+if (k > max){
+    max = k;
+ }
+
+ Console.WriteLine(max);
